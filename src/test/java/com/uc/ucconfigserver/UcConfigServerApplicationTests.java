f6fd34f5e72a356493449dcf17a588a124bdff37
yantaosun <15910344486@163.com>
2020-01-16 16:20:52 +0800

    init

---
+++ src/test/java/com/uc/ucconfigserver/UcConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uc.ucconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
